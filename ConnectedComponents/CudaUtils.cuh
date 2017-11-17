88f56119ff0614970195e400194a7737b201193b
Yngve <yngvenot@gmail.com>
2017-11-17 14:32:05 +0100

    Added check for CUDA errors

--- ConnectedComponents/CudaUtils.cuh
+++ ConnectedComponents/CudaUtils.cuh
@@ -2,21 +2,35 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__device__ unsigned int getGlobalIdx_3D_3D()
+#define CHECK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+
+// Courtesy of "talonmies" https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
 {
-	int blockId = blockIdx.x
+	if (code != cudaSuccess)
+	{
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort) exit(code);
+	}
+}
+
+__device__ unsigned int GetGlobalIdx_3D_3D()
+{
+	const int blockId = blockIdx.x
 		+ blockIdx.y * gridDim.x
 		+ gridDim.x * gridDim.y * blockIdx.z;
-	int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
+	const int threadId = blockId * (blockDim.x * blockDim.y * blockDim.z)
 		+ (threadIdx.z * (blockDim.x * blockDim.y))
 		+ (threadIdx.y * blockDim.x)
 		+ threadIdx.x;
 	return threadId;
+
 }
 
-__device__ unsigned int getNumThreads()
+__device__ unsigned int GetNumThreads()
 {
-	unsigned int numBlocks = gridDim.x * gridDim.y * gridDim.z;
-	unsigned int numThreadsPerBlock = blockDim.x * blockDim.y * blockDim.z;
+	const unsigned int numBlocks = gridDim.x * gridDim.y * gridDim.z;
+	const unsigned int numThreadsPerBlock = blockDim.x * blockDim.y * blockDim.z;
 	return numBlocks * numThreadsPerBlock;
 }
